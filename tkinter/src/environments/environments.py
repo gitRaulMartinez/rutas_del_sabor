324e3119e4c927473c0baadf0c726e2f43e4ffa3
Raul Martinez <rauldeoran17@gmail.com>
2023-08-10 15:38:29 -0300

    add readme update

--- tkinter/src/environments/environments.py
+++ tkinter/src/environments/environments.py
@@ -1,3 +1,6 @@
-URL_SERVER = 'http://localhost:5000'
+URL_SERVER_LOCAL = 'http://localhost:5000'
+URL_SERVER_PRODUCTION = 'https://rutasdelsabor.softwarebasics.com.ar'
+
+URL_SERVER = URL_SERVER_PRODUCTION
 
 URL_GOOGLE_CLOUD = 'https://storage.googleapis.com/rutas-del-sabor/images/'
